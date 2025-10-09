795f35a971693f90f77918f2a5e780bc65a5fe0f
c26jm <jmadover@gmail.com>
2025-10-09 11:40:12 -0400

    most recent update

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
 
 const config = {
-  preprocess: sveltePreprocess(),
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
       fallback: 'index.html'
     }),
+
     paths: {
-      base: dev ? '' : '/DaltonRoboticsSignInWebsite'
-    },
-    prerender: {
-      handleHttpError: 'warn'
+      base: dev ? '' : '/DaltonRoboticsSignInWebsite',
     }
   }
 };
